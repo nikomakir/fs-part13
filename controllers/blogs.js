ea486a2544121a6d3f890284b40bfafc299ff682
Niko Raulas <niko.raulas@outlook.com>
2025-02-01 16:34:25 +0200

    13.5-13.7

---
+++ controllers/blogs.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  const blog = await Blog.create(req.body)
+  res.json(blog)
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+  }
+  res.status(204).end()
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  req.blog.likes ++
+  await req.blog.save()
+  res.json({ likes: req.blog.likes })
+})
+
+module.exports = router
